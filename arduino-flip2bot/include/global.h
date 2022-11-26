0bc42f24b450d84cde6a6874f9e4c33f2c33b75d
Boopesh <105548657+BOOPESH-foxy@users.noreply.github.com>
2022-11-26 22:55:59 +0530

    Add files via upload

---
+++ arduino-flip2bot/include/global.h
@@ -0,0 +1,214 @@
+#include <ESP32Servo.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include "motorGlobal.h"
+#include <math.h>
+#include <Wire.h>
+#include <string.h>
+// #include <Adafruit_TCS34725.h>
+// #include <SPI.h>
+
+//Todo Change Device name, topic, destination topic according to bot number.
+char device_name[] = "ESP32-bot3";
+char device_topic[] = "flipkart/bot3";
+// char destination_topic[] = "flipkart/color2";
+
+void callback(char *topic, byte *message, unsigned int length);
+
+int wifiled = 5;
+
+const int angularPwm = 180;
+const int linearPwm = 200;
+const int lineargoalPwm = 180;
+
+Servo servo;
+
+// //Retriving Color from RGB data from TCS3475 RGB sensor
+
+// int red[] = {127, 85, 85};
+
+// int green[] = {36, 100, 77};
+
+// int dark_blue[] = {31, 74, 121};
+
+// int yellow[] = {91, 85, 50};
+
+// int violet[] = {58, 80, 102};
+
+// int brown[] = {85, 85, 56};
+
+// int white[] = {73, 94, 59};
+
+// int pink[] = {97, 59, 89};
+
+// int orange[] = {191, 31, 31};
+
+// Networking Credentials
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+const char *ssid = "Punctual Puckers";
+const char *password = "teamalpha";
+const char *mqtt_server = "192.168.1.100";
+
+//util compare function
+bool eq(int n, int *x, int *y, int tolerance)
+{
+    const int absTolerance = std::abs(tolerance);
+    for (unsigned int i = 0; i < n; ++i)
+    {
+        if (std::abs(x[i] - y[i]) > absTolerance)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+//setup Ledc
+void setupLedc()
+{
+    ledcSetup(ledChannel, freq, resolution);
+    ledcSetup(ledChannel1, freq, resolution);
+    ledcSetup(ledChannel2, freq, resolution);
+    ledcSetup(ledChannel3, freq, resolution);
+    ledcSetup(ledChannel4, freq, resolution);
+    ledcSetup(ledChannel5, freq, resolution);
+    ledcSetup(ledChannel6, freq, resolution);
+    ledcSetup(ledChannel7, freq, resolution);
+
+    ledcAttachPin(motor4Pin1, ledChannel5); // Motor 4 D26
+    ledcAttachPin(motor4Pin2, ledChannel4); //Motor 4 D27
+
+    ledcAttachPin(motor2Pin1, ledChannel7); // Motor 2 D32
+    ledcAttachPin(motor2Pin2, ledChannel6); // Motor 2 D33
+
+    ledcAttachPin(motor1Pin1, ledChannel3); // Motor 1 D18
+    ledcAttachPin(motor1Pin2, ledChannel2); // Motor 1 D19
+
+    ledcAttachPin(motor3Pin1, ledChannel1); // Motor 3 D12
+    ledcAttachPin(motor3Pin2, ledChannel);  // Motor 3 D13
+
+    pinMode(motor1Pin1, OUTPUT);
+    pinMode(motor1Pin2, OUTPUT);
+
+    pinMode(motor2Pin1, OUTPUT);
+    pinMode(motor2Pin2, OUTPUT);
+
+    pinMode(motor3Pin1, OUTPUT);
+    pinMode(motor3Pin2, OUTPUT);
+
+    pinMode(motor4Pin1, OUTPUT);
+    pinMode(motor4Pin2, OUTPUT);
+
+    pinMode(wifiled, OUTPUT);
+}
+//Setup WiFi properties
+void setup_wifi()
+{
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        digitalWrite(wifiled, HIGH);
+        delay(1000);
+        digitalWrite(wifiled, LOW);
+        delay(1000);
+
+        Serial.print(".");
+    }
+
+    digitalWrite(wifiled, HIGH);
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(mqtt_server);
+}
+//Reconnect to WiFi
+void reconnect()
+{
+    while (!client.connected())
+    {
+
+        Serial.print("Attempting MQTT connection...");
+        if (client.connect(device_name))
+        {
+            Serial.println("connected");
+            client.subscribe(device_topic);
+            Serial.println(device_topic);
+        }
+        else
+        {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" try again in 5 seconds");
+            delay(5000);
+        }
+    }
+}
+
+//Sorting packages with respect to colors
+// int color()
+// {   
+
+//     Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+
+//     float r, g, b;
+
+//     tcs.getRGB(&r, &g, &b);
+
+//     delay(100);
+
+//     Serial.println(int(r));
+//     Serial.println(int(g));
+//     Serial.println(int(b));
+//     Serial.println("-------------------------------------");
+
+//     int currentRGB[] = {int(r), int(g), int(b)};
+
+//     if (eq(2, red, currentRGB, 10))
+//     {
+//         return 1; // PUNE
+//     }
+//     else if (eq(2, green, currentRGB, 20))
+//     {
+//         return 2; // AMEMDABAD
+//     }
+//     else if (eq(2, dark_blue, currentRGB, 10))
+//     {
+//         return 3; // JAIPUR
+//     }
+//     else if (eq(2, yellow, currentRGB, 10))
+//     {
+//         return 4; // CHENNAI
+//     }
+//     else if (eq(2, violet, currentRGB, 10))
+//     {
+//         return 5; // BENGALURU
+//     }
+//     else if (eq(2, brown, currentRGB, 10))
+//     {
+//         return 6; // HYDERABAD
+//     }
+//     else if (eq(2, white, currentRGB, 10))
+//     {
+//         return 7; // MUMBAI
+//     }
+//     else if (eq(2, pink, currentRGB, 10))
+//     {
+//         return 8; // DELHI
+//     }
+//     else if (eq(2, orange, currentRGB, 10))
+//     {
+//         return 9; //KOLKATA
+//     }
+//     else
+//     {
+//         return 0;
+//     }
+// }
---
+++ arduino-flip2bot/src/main.cpp
@@ -0,0 +1,279 @@
+#include "Arduino.h"
+#include "global.h"
+void setup()
+{
+
+  setupLedc();
+
+  Serial.begin(115200);
+
+  servo.attach(15); //D4
+  servo.write(90);
+
+  // Serial.begin(115200);
+
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void callback(char *topic, byte *message, unsigned int length)
+{
+  StaticJsonDocument<256> doc;
+  deserializeJson(doc, message);
+  int angular = doc["angular"];
+  int linear_x = doc["linear_x"];
+  int linear_y = doc["linear_y"];
+  int servo_data = doc["servo"];
+  // int cube = doc["colorRequest"];
+
+  // if (cube == 1)
+  // {
+  //   int colordata = color();
+  //   Serial.println(colordata);
+  //   if (colordata != 0)
+  //   {
+  //     String returnString = String(colordata);
+  //     char destination[10];
+  //     returnString.toCharArray(destination, 10);
+  //     client.publish(destination_topic, destination);
+  //   }
+  // }
+  //Linear pulse
+
+  if (linear_x == 2)
+  {
+    ledcWrite(ledChannel, 0);
+    ledcWrite(ledChannel1, lineargoalPwm);
+
+    ledcWrite(ledChannel2, 0);
+    ledcWrite(ledChannel3, lineargoalPwm);
+
+    ledcWrite(ledChannel4, 0);
+    ledcWrite(ledChannel5, 0);
+
+    ledcWrite(ledChannel6, 0);
+    ledcWrite(ledChannel7, 0);
+    return;
+  }
+
+  if (linear_x == -2)
+  {
+    ledcWrite(ledChannel, lineargoalPwm);
+    ledcWrite(ledChannel1, 0);
+
+    ledcWrite(ledChannel2, lineargoalPwm);
+    ledcWrite(ledChannel3, 0);
+
+    ledcWrite(ledChannel4, 0);
+    ledcWrite(ledChannel5, 0);
+
+    ledcWrite(ledChannel6, 0);
+    ledcWrite(ledChannel7, 0);
+    return;
+  }
+
+  if (linear_y == 2)
+  {
+    ledcWrite(ledChannel, 0);
+    ledcWrite(ledChannel1, 0);
+
+    ledcWrite(ledChannel2, 0);
+    ledcWrite(ledChannel3, 0);
+
+    ledcWrite(ledChannel4, lineargoalPwm);
+    ledcWrite(ledChannel5, 0);
+
+    ledcWrite(ledChannel6, lineargoalPwm);
+    ledcWrite(ledChannel7, 0);
+    return;
+  }
+
+  if (linear_y == -2)
+  {
+    ledcWrite(ledChannel, 0);
+    ledcWrite(ledChannel1, 0);
+
+    ledcWrite(ledChannel2, 0);
+    ledcWrite(ledChannel3, 0);
+
+    ledcWrite(ledChannel4, 0);
+    ledcWrite(ledChannel5, lineargoalPwm);
+
+    ledcWrite(ledChannel6, 0);
+    ledcWrite(ledChannel7, lineargoalPwm);
+    return;
+  }
+
+  // Angular left
+
+  if (angular == 1)
+  {
+    if (abs(linear_y) != 0)
+    {
+      //TODO BOT 3
+      ledcWrite(ledChannel, 0);
+      ledcWrite(ledChannel1, angularPwm);
+      ledcWrite(ledChannel2, angularPwm);
+      ledcWrite(ledChannel3, 0);
+      delay(50);
+      allHalt();
+      return;
+      //TODO BOT N
+ /*      ledcWrite(ledChannel, angularPwm);
+      ledcWrite(ledChannel1, 0);
+      ledcWrite(ledChannel2, 0);
+      ledcWrite(ledChannel3, angularPwm);
+      delay(50);
+      allHalt();
+      return; */
+    }
+    if (abs(linear_x) != 0)
+    {
+      ledcWrite(ledChannel4, 0);
+      ledcWrite(ledChannel5, angularPwm);
+      ledcWrite(ledChannel6, angularPwm);
+      ledcWrite(ledChannel7, 0);
+      delay(50);
+      allHalt();
+      return;
+    }
+  }
+
+  if (angular == -1)
+  {
+    if (abs(linear_y) != 0)
+    {
+      //TODO BOT 3      
+      ledcWrite(ledChannel, angularPwm);
+      ledcWrite(ledChannel1, 0);
+      ledcWrite(ledChannel2, 0);
+      ledcWrite(ledChannel3, angularPwm);
+      delay(50);
+      allHalt();
+      return;
+      //TODO BOT N
+      // ledcWrite(ledChannel, 0);
+      // ledcWrite(ledChannel1, angularPwm);
+      // ledcWrite(ledChannel2, angularPwm);
+      // ledcWrite(ledChannel3, 0);
+      // delay(50);
+      // allHalt();
+      // return;
+    }
+    if (abs(linear_x) != 0)
+    {
+      ledcWrite(ledChannel4, angularPwm);
+      ledcWrite(ledChannel5, 0);
+
+      ledcWrite(ledChannel6, 0);
+      ledcWrite(ledChannel7, angularPwm);
+      delay(50);
+      allHalt();
+      return;
+    }
+  }
+
+  if (angular == 0)
+  {
+    if (linear_x == 0 && linear_y == 0)
+    {
+      allHalt();
+    }
+
+    //forward
+
+    if (linear_y == 1)
+    {
+      ledcWrite(ledChannel, 0);
+      ledcWrite(ledChannel1, 0);
+
+      ledcWrite(ledChannel2, 0);
+      ledcWrite(ledChannel3, 0);
+
+      ledcWrite(ledChannel4, linearPwm);
+      ledcWrite(ledChannel5, 0);
+
+      ledcWrite(ledChannel6, linearPwm);
+      ledcWrite(ledChannel7, 0);
+      return;
+    }
+
+    //backward
+    if (linear_y == -1)
+    {
+      ledcWrite(ledChannel, 0);
+      ledcWrite(ledChannel1, 0);
+
+      ledcWrite(ledChannel2, 0);
+      ledcWrite(ledChannel3, 0);
+
+      ledcWrite(ledChannel4, 0);
+      ledcWrite(ledChannel5, linearPwm);
+
+      ledcWrite(ledChannel6, 0);
+      ledcWrite(ledChannel7, linearPwm);
+      return;
+    }
+
+    if (linear_x == -1)
+    {
+      ledcWrite(ledChannel, linearPwm);
+      ledcWrite(ledChannel1, 0);
+
+      ledcWrite(ledChannel2, linearPwm);
+      ledcWrite(ledChannel3, 0);
+
+      ledcWrite(ledChannel4, 0);
+      ledcWrite(ledChannel5, 0);
+
+      ledcWrite(ledChannel6, 0);
+      ledcWrite(ledChannel7, 0);
+      return;
+    }
+
+    if (linear_x == 1)
+    {
+      ledcWrite(ledChannel, 0);
+      ledcWrite(ledChannel1, linearPwm);
+
+      ledcWrite(ledChannel2, 0);
+      ledcWrite(ledChannel3, linearPwm);
+
+      ledcWrite(ledChannel4, 0);
+      ledcWrite(ledChannel5, 0);
+
+      ledcWrite(ledChannel6, 0);
+      ledcWrite(ledChannel7, 0);
+      return;
+    }
+  }
+
+  if (servo_data == 1)
+  {
+    servo.write(0);
+    // Serial.println("its 0 degrees");
+    delay(1000);
+    servo.write(90);
+    // Serial.println("its 90 degrees");
+    delay(1000);
+  }
+  if (servo_data == -1)
+  {
+    servo.write(180);
+    // Serial.print("its 150 degrees");
+    delay(1000);
+    servo.write(90);
+    // Serial.println("its 90 degrees");
+    delay(1000);
+  }
+}
+
+void loop()
+{
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  client.loop();
+}
